a98dfacf21de48d570741bf27fe7af9788929e46
Andre Luiz <adventureandre@hotmail.com>
2024-02-14 23:22:28 -0300

    releases: Estrutura da aplicacao

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app =  fastify()
+
+app.get('/hello', (req, res) => {
+    return "hello world"
+})
+
+
+app.listen({
+    port:3333,
+}).then(    ( )=>{
+    console.log("HTTP Server Running")
+    
+})
