26f6e6e6427ebd5f31fc9f9c30bc5e3b4969fb77
Andre Luiz <adventureandre@hotmail.com>
2024-02-15 21:54:00 -0300

    Configurando o Knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', (req, res) => {
-  return 'hello world'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
@@ -11,5 +14,5 @@ app
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP Server Running')
+    console.log('HTTP Server Running!')
   })
