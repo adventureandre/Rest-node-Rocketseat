e56100daaff5530af980e7bd51b2e0916fe795e0
Andre Luiz <adventureandre@hotmail.com>
2024-04-14 15:44:01 -0300

    fix bug

--- db/migrations/20240223015518_add-session-id-to-transaction.ts
+++ db/migrations/20240223015518_add-session-id-to-transaction.ts
@@ -1,16 +1,13 @@
-import type { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.alterTable('transactions',(table)=>{
-        table.uuid('session_id').after('id').index()
-    })
+  await knex.schema.alterTable('transactions', (table) => {
+    table.uuid('session_id').after('id').index()
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.alterTable('transactions',(table)=>{
-        table.dropColumn('session_id')
-    })
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id')
+  })
 }
-
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,21 @@
-import fastify from "fastify"
-import { knex } from "./database"
-import crypto from "node:crypto"
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
-const app =  fastify()
+const app = fastify()
 
-app.get("/hello", async ( ) => {
-//    const transaction = await knex('transactions').insert({
-//     id:crypto.randomUUID(),
-//     title: "transaction de teste",
-//     amount: 1000,
-//    }).returning("*")
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-//    return transaction
-
-const transactions = await knex('transactions')
-.select('*')
-.where('amount', 1000)
-return transactions
-
+  return transactions
 })
 
-
-app.listen({
-    port:3333,
-}).then(    ( )=>{
-    console.log("HTTP Server Running")
-    
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
